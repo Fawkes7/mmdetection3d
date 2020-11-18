9a7ce45ae6c22c54bef84de8b6601f2f3332b2df
Cao Haoyuan <cao.haoyuan1@bytedance.com>
2020-11-18 00:55:39 -0800

    remove test code

--- torch_testcode.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-indices = torch.tensor([[0, 1], [2, 3]])
-updates = torch.tensor([[5, 5, 5, 5],
-                        [6, 6, 6, 6]])
-result = torch.zeros((4, 4, 4), dtype = torch.int64)
-result[indices[:, 0], indices[:, 1]] = updates
-print("PyTorch")
-print(result.numpy())
