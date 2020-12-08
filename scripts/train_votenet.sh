8e04b2cb0960d215b29371ced36be9d5a2fb51cf
Cao Haoyuan <cao.haoyuan1@bytedance.com>
2020-12-07 17:07:37 -0800

    personalized data path

--- scripts/train_votenet.sh
+++ scripts/train_votenet.sh
@@ -7,7 +7,6 @@ MMDETPATH="/home/haoyuan/mmdet/mmdetection3d"
 cd $MMDETPATH
 
 RANK=0 python3.6 -m tools.train ${BASE}${CONFIG} --work-dir=log
-=======
 cd /home/lz/data/Projects/Vision/3D/github_resources/mmdetection3d
 #RANK=0 python -m tools.train ../../code/working_code/configs/votenet_partnet_config.py --work-dir=log/Laptop-1
 #RANK=0 python -m tools.train ../../code/working_code/configs/votenet_partnet_config.py --work-dir=log/Keyboard-1
@@ -15,4 +14,3 @@ RANK=0 python -m tools.train ../../code/working_code/configs/votenet_partnet_con
 
 #RANK=0 python -m tools.train ../../code/working_code/old_config/votenet_partnet_config.py --work-dir=log
 cd ../../code/working_code
->>>>>>> acd8ddf40dc7fd917025f70cfcf847a53493fa28
